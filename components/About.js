87749b08a19a08559c99b19c78bc75709e8aa6a7
Maciej Jesiołowski <46161618+mjesiolowski@users.noreply.github.com>
2020-09-26 18:24:30 +0200

    Added About component (#4)
    * added About component boilerplate
    
    * simplified aboutUs query

---
+++ components/About.js
@@ -0,0 +1,19 @@
+import { useQuery } from '@apollo/react-hooks';
+import ReactMarkdown from 'react-markdown';
+import { GET_ABOUT_US } from '../lib/queries';
+
+const About = () => {
+  const { loading, data, error } = useQuery(GET_ABOUT_US);
+
+  const { aboutUs } = data?.aboutCollection?.items[0] || {};
+  console.log({ aboutUs });
+
+  return (
+    <>
+      <div>ABOUT</div>
+      <ReactMarkdown source={aboutUs} />
+    </>
+  );
+};
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import CollectionCardList from '../components/CollectionCardList';
+import About from '../components/About';
 import withApollo from '../lib/apollo';
 
 const App = () => (
-  <CollectionCardList />
+  <>
+    <CollectionCardList />
+    <About />
+  </>
 );
 
 export default withApollo()(App);
