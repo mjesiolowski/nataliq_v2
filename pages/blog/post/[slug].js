515e1aa91899dfb4a1296ee73b1e5260d5b72c24
Maciej Jesiolowski <maciej.jesiolowski@gmail.com>
2020-12-23 17:20:11 +0100

    WIP

--- pages/blog/post/[slug].js
+++ pages/blog/post/[slug].js
@@ -1,15 +1,21 @@
+import { getBlogPost } from '../../../lib/collection';
+
 export async function getServerSideProps({ params }) {
   const { slug } = params;
 
+  const blogTitle = (slug.charAt(0).toUpperCase() + slug.slice(1)).split('-').join(' ');
+  const blogPostData = await getBlogPost(blogTitle);
+
   return {
     props: {
-      slug,
+      blogPostData,
     },
   };
 }
 
-const BlogPost = ({ slug }) => {
-  console.log({ slug });
+const BlogPost = ({ blogPostData }) => {
+  const { content, title, image } = blogPostData;
+  console.log({ content, title, image });
   return (
     <p>BLOGPOST</p>
   );
